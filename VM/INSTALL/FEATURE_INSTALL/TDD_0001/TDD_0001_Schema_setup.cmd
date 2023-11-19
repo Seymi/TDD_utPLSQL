18c9ffed76ddcb34ca651e3abf53e81591e7608a
Martin Seymann <martin.seymann@chello.at>
2023-11-19 05:14:16 +0100

    setup

--- VM/INSTALL/FEATURE_INSTALL/TDD_0001/TDD_0001_Schema_setup.cmd
+++ VM/INSTALL/FEATURE_INSTALL/TDD_0001/TDD_0001_Schema_setup.cmd
@@ -10,16 +10,14 @@ REM install in schema seymi
 set user=seymi
 
 sqlplus %user%@%server%/%instanz% @%feature_install_dir%\tdd_0001\TDD_0001_master_install.sql
-
-rem sqlplus %user%/fl1wsal@%server%/%instanz% @%feature_install_dir%\cleanup.sql
-rem sqlplus %user%/fl1wsal@%server%/%instanz% @%feature_install_dir%\TDD_0001_install.sql
+rem pwd from commandline (std pwd)
 disconnect
 
 rem drop table test;
 rem @%feature_install_dir%\create_table_test.sql
 
-rem sqlplus seymi/fl1wsal@192.168.7.1/orcl @D:\deploy\Oracle_Developer_VM\feature_install\cleanup.sql
-rem sqlplus seymi/fl1wsal@192.168.7.1/orcl @D:\deploy\Oracle_Developer_VM\feature_install\create_table_test.sql
-rem sqlplus seymi/fl1wsal@192.168.7.1/orcl @create_table_test.sql
+rem sqlplus seymi/<pwd>@192.168.7.1/orcl @D:\deploy\Oracle_Developer_VM\feature_install\cleanup.sql
+rem sqlplus seymi/<pwd>@192.168.7.1/orcl @D:\deploy\Oracle_Developer_VM\feature_install\create_table_test.sql
+rem sqlplus seymi/<pwd>@192.168.7.1/orcl @create_table_test.sql
 
 echo on
---
+++ VM/SEYMI/DML/text_utf8.txt
@@ -0,0 +1,10 @@
+Da sind Umlaute enthalten
+Österreich 
+Überall
+Glücklich
+Ähnlich
+Löcher
+Straße
+Krähe
+Euro € Symbol
+Back \ Slash
